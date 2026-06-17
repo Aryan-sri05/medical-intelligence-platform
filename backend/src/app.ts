8e8a9ed30e1e45636eb748d77342d628267f68d9
Aryan Srivastava <asrivastava4_be22@thapar.edu>
2026-06-17 23:03:55 +0530

    railway port fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,8 +33,7 @@ app.use("/cdc", cdcRoutes);
 app.use("/medrxiv", medrxivRoutes);
 app.use("/biorxiv", biorxivRoutes);
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
